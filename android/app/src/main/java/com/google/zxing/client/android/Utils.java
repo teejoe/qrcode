d057b1c284f70fbe24b796ff464a4032c36e0849
maotianjiao <gzmaotianjiao@corp.netease.com>
2017-11-08 15:03:27 +0800

    add Contrasted & Downscaled luminance source

--- android/app/src/main/java/com/google/zxing/client/android/Utils.java
+++ android/app/src/main/java/com/google/zxing/client/android/Utils.java
@@ -6,6 +6,7 @@ import android.graphics.Color;
 
 import com.google.zxing.BinaryBitmap;
 import com.google.zxing.Logging;
+import com.google.zxing.LuminanceSource;
 import com.google.zxing.NotFoundException;
 
 import java.io.File;
@@ -30,6 +31,22 @@ public class Utils {
                 } else {
                     bitmap.setPixel(i, j, Color.WHITE);
                 }
+            }
+        }
+        Logging.d("convert cost:" + (System.currentTimeMillis() - start));
+        return bitmap;
+    }
+
+    public static Bitmap luminanceSource2Bitmap(LuminanceSource source) {
+        long start = System.currentTimeMillis();
+        int width = source.getWidth();
+        int height = source.getHeight();
+        byte[] matrix = source.getMatrix();
+        Bitmap bitmap = Bitmap.createBitmap(width, height, Config.ARGB_8888);
+        for (int i = 0; i < width; i++) {
+            for (int j = 0; j < height; j++) {
+                int value = matrix[j * height + i] & 0xff;
+                bitmap.setPixel(i, j, Color.rgb(value, value, value));
             }
         }
         Logging.d("convert cost:" + (System.currentTimeMillis() - start));
